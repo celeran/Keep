e4d33fc72213623ff76b7eee7026ebf1d48e6587
celeran <69913111+celeran@users.noreply.github.com>
2020-10-06 20:58:36 +0300

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/k1odev/keep/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/k1odev/keep/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.wil8dev.keep", appContext.packageName)
+        assertEquals("com.k1odev.keep", appContext.packageName)
     }
 }
